2cf8b9fdb75e6c027cf167eb1af1612480e41d80
Karina <>
2024-09-15 12:50:39 +0300

    new commit

---
+++ src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        ServerWindow serverWindow = new ServerWindow();
+        new ClientGUI(serverWindow);
+        new ClientGUI(serverWindow);
+    }
+}
